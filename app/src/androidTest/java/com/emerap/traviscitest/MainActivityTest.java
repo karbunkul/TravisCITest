f1092622d99e3e0c5dbdbe6990628c60c24e72c8
Pokhodyun Alexander <karbunkul@yourtask.ru>
2017-01-23 22:59:59 +0500

    codecov test

---
+++ app/src/androidTest/java/com/emerap/traviscitest/MainActivityTest.java
@@ -0,0 +1,28 @@
+package com.emerap.traviscitest;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by karbunkul on 23.01.17.
+ */
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldShow42() {
+        // then
+        onView(withText("42")).check(matches(isDisplayed()));
+    }
+
+}
